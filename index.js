3751f2a2671f2691868cbdf8c4f0c451a57141f6
foysal767 <uddinmohammadfoysal981@gmail.com>
2022-11-28 23:06:12 +0600

    api created for delete product

--- index.js
+++ index.js
@@ -141,6 +141,13 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/myproducts/:id', async(req, res)=> {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const result = await productsCollection.deleteOne(query)
+            res.send(result)
+        })
+
     }
     finally {
 
